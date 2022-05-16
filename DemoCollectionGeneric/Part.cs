3f2140186d2b2f0a0c2e1214c4f5b3b3a2996e22
Dinh Tien <82441050+dinhtiendev@users.noreply.github.com>
2022-05-16 20:26:30 +0700

    add project demoCollections and demoCollectionsGeneric

---
+++ DemoCollectionGeneric/Part.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DemoCollectionGeneric
+{
+    internal class Part : IEnumerable<Part>
+    {
+        public string PartName { get; set; }
+
+        public int PartId { get; set; }
+
+        public override string ToString()
+        {
+            return "ID: " + PartId + "   Name: " + PartName;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Part objAsPart = obj as Part;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+        public override int GetHashCode()
+        {
+            return PartId;
+        }
+        public bool Equals(Part other)
+        {
+            if (other == null) return false;
+            return (this.PartId.Equals(other.PartId));
+        }
+
+        public IEnumerator<Part> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Slot4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Slot4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates and initializes a new ArrayList.
+            ArrayList myAL = new ArrayList();
+            myAL.Add("Hello");
+            myAL.Add("World");
+            myAL.Add("!");
+
+            // Displays the properties and values of the ArrayList.
+            Console.WriteLine("myAL");
+            Console.WriteLine("    Count:    {0}", myAL.Count);
+            Console.Write("    Values:");
+            foreach (Object obj in myAL)
+                Console.Write("   {0}", obj);
+            Console.WriteLine();
+        }
+    }
+}
